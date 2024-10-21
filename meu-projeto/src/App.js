c9bc6e17d48e5947d71155e3522d0a5137fe67f3
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-10-21 18:25:35 -0300

    react

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
-
+import Frase from './components/Frase';
 function App() {
 
   const nome = 'Maria'
   return (
     <div className="App">
+      <Frase/>
       <HelloWorld/>
       <SayMyName nome="Matheus"/>
       <SayMyName nome="Pedro"/>
@@ -15,7 +16,7 @@ function App() {
       <Pessoa nome="Matheus"
       idade="16" 
       profissao="programador" 
-      foto="https://via.placeholdes.com/150"
+      foto="https://via.placeholder.com/150"
       />
     </div>
   );
